1b03b470d5de4ffafe7016b4ed20a8708f02c060
NastyaTMF <onvard@gmail.com>
2016-01-29 13:17:27 +0300

    Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,31 @@
-"use strict";
-module.exports = function(grunt) {
-  require("load-grunt-tasks")(grunt);
-
-  var config = {
-
-    pkg: grunt.file.readJSON("package.json"),
-
-    less: {
-      style: {
-        files: {
-          "css/style.css": ["source/less/style.less"]
-        }
-      }
-    },
-
-    postcss: {
-      options: {
-        processors: [
-          require("autoprefixer")({browsers: "last 20 versions"})
-        ]
-      },
-      style: {
-        src: "build/css/*.css"
-      }
-    },
-
-    watch: {
-      style: {
-        files: ["less/**/*.less"],
-        tasks: ["less", "postcss"],
-        options: {
-          spawn: false,
-          livereload: true
-        }
-      }
-    },
-
-    clean: {
+'use strict';
+ 
+module.exports = function (grunt) {
+    // load all grunt tasks
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+         watch: {
+          style: {
+            files: ["less/**/*.less"],
+            tasks: ["less", "postcss"],
+            options: {
+              spawn: false,
+              livereload: true
+            }
+         }
+        },
+        // "less"-task configuration
+        less: {
+          style: {
+            files: {
+              "source/css/style.css": ["source/less/style.less"]
+            }
+          }
+        },
+  clean: {
         build: ["build"]
     },
 
@@ -55,73 +44,7 @@ module.exports = function(grunt) {
             }]
         }
     },
-
-    cmq: {
-       style: {
-           files: {
-               "build/css/style.css": ["build/css/style.css"]
-           }
-       }
-    },
-
-    imagemin: {
-       images: {
-           options: {
-               optimizationLevel: 3
-           },
-           files: [{
-               expand: true,
-               src: ["build/img/**/*.{img,jpg,gif,svg}"]
-           }]
-       }
-    },
-
-    cssmin: {
-        options: {
-            keepSpecialComments: 0,
-            report: "gzip"
-        },
-        style: {
-            files: {
-                "build/css/style.min.css": ["build/css/style.css"]
-            }
-        }
-    },
-
-    csscomb: {
-        dist: {
-            options: {
-                config: '.csscomb.json'
-            },
-            files: {
-                'css/style.css': ['css/style.css']
-            }
-        }
-    },
-    uglify: {
-        options: {
-            mangle: false
-        },
-        my_target: {
-            files: {
-                'build/js/script.min.js': ['source/js/script.js']
-            }
-        }
-    }
-
-  };
-
-  grunt.initConfig(config);
-
-  grunt.registerTask("build", [
-      "clean",
-      "copy",
-      "less",
-      "cmq",
-      "postcss",
-      "csscomb",
-      "imagemin",
-      "cssmin",
-      "uglify"
-  ]);
+    });
+     // the default task (running "grunt" in console) is "watch"
+grunt.registerTask("build", ["clean", "copy", "less"]);
 };
